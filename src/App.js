b50892706a6cf6e7c3647c21f40a2bf918c396bb
Y M V Surya Narayana <123289870+hiddentigersurya@users.noreply.github.com>
2024-09-14 13:37:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import StorageComponent from './components/StorageComponent/StorageComponent';
 import Profiles from './components/Profiles/Profiles';
 import VoiceAIComponent from './components/VoiceAIComponent/VoiceAiComponent';
 import './Home.css';
+import ParticlesComponent from './components/particles';
 import ClientDataset from './components/ClientDataset/ClientDataset';
 import ClientProfileView from './components/ClientProfileView/ClientProfileView';
 import Login from './components/Login/Login';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import LandingPage from "./components/LandingPage/LandingPage"
-
 import AdminDashboard from './components/AdminPage/AdminDashboard';
 
 const App = () => {
@@ -22,24 +22,27 @@ const App = () => {
     }, []);
 
     return (
-        <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_CLIENTID}`}>
-            <BrowserRouter>
-                <ThemeProvider>
-                    <Routes>
-                        {/*<Route path='/login' element={<Login />} />*/}
-                        <Route path='/' element={<LandingPage />} />
-                        <Route path='/admindashboard' element={<AdminDashboard />} />
-                        <Route path="/voiceAi" element={<ProtectedRoute element={VoiceAIComponent} />} />
-                        <Route path="/chat" element={<ProtectedRoute element={Home} />} />
-                        <Route path='/dataset' element={<ProtectedRoute element={StorageComponent} />} />
-                        <Route path="/clientDataset" element={<ProtectedRoute element={ClientDataset} />} />
-                        <Route path="/clientprofile/:id" element={<ProtectedRoute element={ClientProfileView} />} />
-                        <Route path="/search" element={<ProtectedRoute element={Search} />} />
-                        <Route path="/profiles" element={<ProtectedRoute element={Profiles} />} />
-                    </Routes>
-                </ThemeProvider>
-            </BrowserRouter>
-        </GoogleOAuthProvider>
+        <div className="App">
+            <ParticlesComponent id="particles" />
+            <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_CLIENTID}`}>
+                <BrowserRouter>
+                    <ThemeProvider>
+                        <Routes>
+                            <Route  path='/login' element={<Login />} /> 
+                            <Route path='/' element={<LandingPage />} />
+                            <Route path='/admindashboard' element={<AdminDashboard />} />
+                            <Route path="/voiceAi" element={<ProtectedRoute element={VoiceAIComponent} />} />
+                            <Route path="/chat" element={<ProtectedRoute element={Home} />} />
+                            <Route path='/dataset' element={<ProtectedRoute element={StorageComponent} />} />
+                            <Route path="/clientDataset" element={<ProtectedRoute element={ClientDataset} />} />
+                            <Route path="/clientprofile/:id" element={<ProtectedRoute element={ClientProfileView} />} />
+                            <Route path="/search" element={<ProtectedRoute element={Search} />} />
+                            <Route path="/profiles" element={<ProtectedRoute element={Profiles} />} />
+                        </Routes>
+                    </ThemeProvider>
+                </BrowserRouter>
+            </GoogleOAuthProvider>
+        </div>
     );
 };
 
