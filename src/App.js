b5f64dbe2d2e2c8d32aac8d63564471eb6fc0054
Y M V Surya Narayana <123289870+hiddentigersurya@users.noreply.github.com>
2024-09-14 20:56:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import StorageComponent from './components/StorageComponent/StorageComponent';
 import Profiles from './components/Profiles/Profiles';
 import VoiceAIComponent from './components/VoiceAIComponent/VoiceAiComponent';
 import './Home.css';
-import ParticlesComponent from './components/particles';
 import ClientDataset from './components/ClientDataset/ClientDataset';
 import ClientProfileView from './components/ClientProfileView/ClientProfileView';
 import Login from './components/Login/Login';
@@ -15,20 +14,21 @@ import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import LandingPage from "./components/LandingPage/LandingPage"
 import AdminDashboard from './components/AdminPage/AdminDashboard';
+import Blog from './components/Blog/Blog';
+import About from './components/About/About';
 
 const App = () => {
     useEffect(() => {
-        document.title = 'Legalee Ai';
+        document.title = 'Aishaala';
     }, []);
 
     return (
         <div className="App">
-            <ParticlesComponent id="particles" />
             <GoogleOAuthProvider clientId="722452147364-o9b63kqli85r7l2tsndons0o3o6l8tg4.apps.googleusercontent.com">
                 <BrowserRouter>
                     <ThemeProvider>
                         <Routes>
-                            <Route  path='/login' element={<Login />} /> 
+                            <Route exact path='/login' element={<Login />} /> 
                             <Route path='/' element={<LandingPage />} />
                             <Route path='/admindashboard' element={<AdminDashboard />} />
                             <Route path="/voiceAi" element={<ProtectedRoute element={VoiceAIComponent} />} />
@@ -38,6 +38,8 @@ const App = () => {
                             <Route path="/clientprofile/:id" element={<ProtectedRoute element={ClientProfileView} />} />
                             <Route path="/search" element={<ProtectedRoute element={Search} />} />
                             <Route path="/profiles" element={<ProtectedRoute element={Profiles} />} />
+                            <Route  path="/blog" element={<ProtectedRoute element={Blog} />} />
+                            <Route  path="/about" element={<ProtectedRoute element={About} />} />
                         </Routes>
                     </ThemeProvider>
                 </BrowserRouter>
